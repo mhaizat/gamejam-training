6b6a00dcb1143b98c7483399dc0cc8d316a6566d
scarffy <34879344+scarffy@users.noreply.github.com>
2024-06-20 07:13:30 +0800

    feat: setup settings basic

---
+++ Assets/Scripts/Settings/DisplaySettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplaySettings : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown windowsModeDropdown;
+    [SerializeField] private TMP_Dropdown vsyncDropdown;
+    [SerializeField] private TMP_Dropdown frameRateDropdown;
+}
