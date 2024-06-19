5abf17781cd6b11d0bcbbd1ffc5c2c2059081636
scarffy <34879344+scarffy@users.noreply.github.com>
2024-06-19 20:59:08 +0800

    feat: add pause menu ui flow

--- Assets/Scripts/UIs/UIPanelController/UPCPause.cs
+++ Assets/Scripts/UIs/UIPanelController/UPCPause.cs
@@ -1,7 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UPCPause : UIPanelController
 {
+    [Header("Button Reference")]
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button endButton;
+
+    public override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        resumeButton.onClick.AddListener(ResumeGame);
+        settingButton.onClick.AddListener(OpenSettingsMenu);
+        endButton.onClick.AddListener(EndGame);
+    }
+
+    private void ResumeGame()
+    {
+        UIManager.Instance.SetUIState(UIState.GameHud);
+    }
+
+    private void OpenSettingsMenu()
+    {
+        UIManager.Instance.SetUIState(UIState.Settings);
+    }
+
+    private void EndGame()
+    {
+        UIManager.Instance.SetUIState(UIState.MainMenu);
+    }
 }
